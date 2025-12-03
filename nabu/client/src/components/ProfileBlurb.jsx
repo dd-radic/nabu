bf7fb4ea72e98bc011b2c012fdb50a9457cd091e
dd-radic <radida01@thu.de>
2025-12-03 02:25:12 +0100

    fixed issue with ProfileBlurb

--- nabu/client/src/components/ProfileBlurb.jsx
+++ nabu/client/src/components/ProfileBlurb.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useAuth} from '../AuthProvider';
+import { useEffect } from 'react';
 //import {useState} from 'react';
 
 const ProfileBlurb = () => {
-  const {user, email} = useAuth();
+  const [userdata, setUserdata] = useState([]);
+  const {token} = useAuth();
+   useEffect(() => {
+          const fetchUserdata = async () => {
+              try {
+                  console.log(token);
+                  const res = await fetch("http://localhost:5000/api/user", {
+                  method: 'GET',
+                  headers: {
+                      'Authorization': `Bearer ${token}`,  // Include the token in the request header
+                  },
+              });
+              const data = await res.json();
+              setUserdata(data);
+              } catch (err) {
+                  console.error("AAAAA to load AAASSADA:", err);
+              }
+          };
+  
+          fetchUserdata();
+      }, [token]);
+  
   return (
     <div className="container header-content">
-      {user} - {email}
+      {userdata.name} - {userdata.mail}
     </div>
   );
 };
