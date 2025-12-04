40bb4afc966f065bdfcdc1432fcb74ec7ade63c9
Katie Borromeo <borromkj@rose-hulman.edu>
2025-12-04 14:57:18 +0100

    Refactored AuthProvider.js so that controllers exist on the front-end for better encapsulation

---
+++ nabu/client/src/controllers/loginController.js
@@ -0,0 +1,39 @@
+const loginAction = async (req, setUserdata, setToken) => {
+    try {
+        const res = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(req)
+        });
+
+        if (!res.ok) {
+            alert("Server Error");
+            console.error(`Error logging in: ${res.status}`);
+            return; 
+        }
+
+        const data = await res.json();
+
+        if (data){
+            setUserdata(data)
+            setToken(data.token)
+            localStorage.setItem("site", data.token);
+
+            window.location.href = "/#/dashboard"
+            return;
+        }
+        throw new Error(data.message);
+    } 
+    catch (err) {
+        console.error(err);
+    }
+};
+
+const logOut = (setUserdata) => {
+    setUserdata([])
+    localStorage.removeItem("site");
+    window.location.href = "/#/"
+};
+
+const loginController = {loginAction, logOut}
+export default loginController;
