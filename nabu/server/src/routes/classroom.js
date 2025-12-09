b876df9f1e03819c50f217efebc4f6b59c511bcb
Katie Borromeo <borromkj@rose-hulman.edu>
2025-12-09 08:24:51 +0100

    FIxed errors in user join/leave caused by the merge

--- nabu/server/src/routes/classroom.js
+++ nabu/server/src/routes/classroom.js
@@ -71,14 +71,13 @@ router.post("/add", async (req, res) => {
 // USER JOINS CLASSROOM 
 // ========================================================
 router.post("/join", async (req, res) => {
-  const classRoomId = req.query.classRoomId;
-  const userId = req.query.userId;
+  const {userId, classroomId} = req.body;
 
   try {
     // 1) classroom exists?
     const [cRows] = await pool.query(
       "SELECT OwnerId FROM ClassRoom WHERE Id = ?",
-      [classRoomId]
+      [classroomId]
     );
 
     if (cRows.length === 0) {
@@ -95,7 +94,7 @@ router.post("/join", async (req, res) => {
     // 2) check if user already joined
     const [mRows] = await pool.query(
       "SELECT * FROM UserClassroom WHERE ClassRoomId = ? AND UserId = ?",
-      [classRoomId, userId]
+      [classroomId, userId]
     );
 
     if (mRows.length > 0) {
@@ -105,7 +104,7 @@ router.post("/join", async (req, res) => {
     // 3) insert membership
     await pool.query(
       "INSERT INTO UserClassroom (UserId, ClassRoomId, JoinedAt) VALUES (?, ?, NOW())",
-      [userId, classRoomId]
+      [userId, classroomId]
     );
 
     return res.status(201).json({ message: "Joined classroom" });
@@ -120,14 +119,13 @@ router.post("/join", async (req, res) => {
 // USER LEAVES CLASSROOM 
 // ========================================================
 router.delete("/leave", async (req, res) => {
-  const classRoomId = req.query.classRoomId;
-  const userId = req.query.userId;
+  const {userId, classroomId} = req.body;
 
   try {
     // 1) classroom exists?
     const [cRows] = await pool.query(
       "SELECT OwnerId FROM ClassRoom WHERE Id = ?",
-      [classRoomId]
+      [classroomId]
     );
 
     if (cRows.length === 0) {
@@ -144,7 +142,7 @@ router.delete("/leave", async (req, res) => {
     // 2) check membership
     const [mRows] = await pool.query(
       "SELECT * FROM UserClassroom WHERE UserId = ? AND ClassRoomId = ?",
-      [userId, classRoomId]
+      [userId, classroomId]
     );
 
     if (mRows.length === 0) {
@@ -154,7 +152,7 @@ router.delete("/leave", async (req, res) => {
     // 3) remove membership
     await pool.query(
       "DELETE FROM UserClassroom WHERE UserId = ? AND ClassRoomId = ?",
-      [userId, classRoomId]
+      [userId, classroomId]
     );
 
     return res.status(200).json({ message: "Left classroom successfully" });
@@ -189,7 +187,9 @@ router.get("/isMember", async (req, res) => {
   }
 });
 
+// ========================================================
 //DELETE a classroom
+// ========================================================
 router.delete("/delete", async(req, res) => {
   try {
     const {classroomId} = req.query;
