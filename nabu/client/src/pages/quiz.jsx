0f4764c0333f724b23045b26caf6f42cf20297a7
dd-radic <radida01@thu.de>
2025-12-11 02:20:36 +0100

    added dummy files for questions and quizzes

---
+++ nabu/client/src/pages/quiz.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+/**
+ * =========    COPIED FROM HOME.JSX    ===========
+ * =========    FIX COMMENTS TO MATCH   ===========
+ * 
+ * 
+ * This is the Home Page component.
+ * It renders the main "Welcome" message and the action buttons.
+ */
+const Home = () => {
+
+  return (
+    // This main container fills the remaining screen space
+    <main className="home-content-section">
+      {/* This new container forces all content inside to be centered */}
+      <div className="home-content-wrapper">
+        <h1>
+          WELCOME
+          <br />
+          TO NABU
+        </h1>
+        <p>
+          Your voice. Your solution. Your community.
+        </p>
+
+        {/* This group holds the action buttons */}
+        <div className="home-actions-group">
+          {/* This is the "Sign In" button.
+            It uses the ".btn-brand" class to get the gradient.
+          */}
+          <a href="/#/login" className="btn btn-brand">
+            Log In
+          </a>
+          {/* This is the "Sign Up" button.
+            It uses ".btn-secondary" to be the white/outline style.
+          */}
+          <a href="/#/signup" className="btn btn-secondary">
+            Sign Up
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
